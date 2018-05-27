7dde2b8b102a0f516788e30215275766e2c613ec
wataru86 <is151086@cis.aichi-pu.ac.jp>
2018-05-27 19:02:27 +0900

    E

--- sh.sh
+++ sh.sh
@@ -23,7 +23,7 @@ git add package.yaml
 git commit -m "Release Version $1"
 
 git co master
-git merge -e develop
+git merge --no-edit develop
 
-git co develop
-git rebase master
+#git co develop
+#git rebase master
